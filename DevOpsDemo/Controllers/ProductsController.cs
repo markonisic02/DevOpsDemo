b43704ef57e7b4a8540b4cf2610dc178bb9812be
Marko Nisic <markonisic02@gmail.com>
2026-07-02 19:40:41 +0200

    Initial commit

---
+++ DevOpsDemo/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using DevOpsDemo.Data;
+using DevOpsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevOpsDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Product>> Get()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+
+        return Ok(product);
+    }
+}
---
+++ DevOpsDemo/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DevOpsDemo.Models;
+
+namespace DevOpsDemo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
